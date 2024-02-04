b94895e62064b152e8cbb5e18c7f2d2b4e06ba21
Tomer Admon <tomer.a@qualisystems.com>
2024-02-04 10:59:24 +0200

    Update release.sh

--- assets/scripts/release.sh
+++ assets/scripts/release.sh
@@ -1,6 +1,3 @@
-echo env Provided: ${ENV} 
-echo env Provided: ${TOKEN} 
-
 curl --request DELETE \
   --url https://review2.qualilabs.net/api/spaces/space_name/environments/${ENV}/release \
   --header 'Accept: application/json' \
